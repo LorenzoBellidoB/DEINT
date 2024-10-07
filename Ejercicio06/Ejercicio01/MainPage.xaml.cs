f48ae531a2baff3576d2b1d969547cb3180e8625
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-10-07 14:39:56 +0200

    botton

---
+++ Ejercicio06/Ejercicio01/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Android.App.Admin;
+
+namespace Ejercicio01
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onClickedBtn2()
+
+    }
+
+}
