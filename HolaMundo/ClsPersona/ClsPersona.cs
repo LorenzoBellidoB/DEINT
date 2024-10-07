a6603d0114e0c4b2c5182eb046061a8949f0db11
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-10-07 14:00:30 +0200

    mit

---
+++ HolaMundo/ClsPersona/ClsPersona.cs
@@ -0,0 +1,9 @@
+﻿namespace Persona
+{
+    public class ClsPersona
+    {
+        public string Nombre { get; set; }
+
+        public string Apellidos {  get; set; }
+    }
+}
--- HolaMundo/MauiHolaMundo/MainPage.xaml.cs
+++ HolaMundo/MauiHolaMundo/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiHolaMundo
+﻿using Persona;
+
+namespace MauiHolaMundo
 {
     public partial class MainPage : ContentPage
     {
@@ -9,15 +11,25 @@
             InitializeComponent();
         }
 
-        private async Task Prompt()
-        {
-            string result = await DisplayPromptAsync("", "Introduce tus apellidos");
-        }
 
-        private void OnSaludoClicked(object sender, EventArgs e)
+        private async void OnSaludoClicked(object sender, EventArgs e)
         {
-            result = Prompt();
-            saludo.Text = saludo.Text + nombre.Text + result;
+            
+            string apellidos = await DisplayPromptAsync("Apellidos", "Introduce tus apellidos");
+            ClsPersona persona = new ClsPersona();
+            persona.Nombre = nombre.Text.ToString();
+            persona.Apellidos = apellidos;
+
+            if(!String.IsNullOrEmpty(persona.Nombre) && !String.IsNullOrEmpty(persona.Apellidos))
+            {
+                saludo.Text += nombre.Text + " " + apellidos;
+                saludo.TextColor = Colors.Black;
+            }
+            else
+            {
+                saludo.Text = "Campos incompletos.";
+                saludo.TextColor = Colors.Red;
+            }
         }
     }
 
