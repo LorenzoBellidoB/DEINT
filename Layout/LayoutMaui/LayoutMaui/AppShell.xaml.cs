d780379c567e30e86105899c9cbae62805a00d02
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-10-16 14:19:52 +0200

    Layout

---
+++ Layout/LayoutMaui/LayoutMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LayoutMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Layout/LayoutMaui/LayoutMaui/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LayoutMaui
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
