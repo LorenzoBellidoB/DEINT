94ae7c05045b299b83c0b6d596098bd008a06bff
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-10-18 14:21:56 +0200

    hector lechero

---
+++ Layout/LayoutMaui/Ejercicio4/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Ejercicio4
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
