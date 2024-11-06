1b945a6da5404f0fd390b97b082fe39962d493cb
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-11-06 09:31:03 +0100

    fin

--- PlacasSolares/PlacasSolares/PlacasSolares/Views/ClienteDetalle.xaml.cs
+++ PlacasSolares/PlacasSolares/PlacasSolares/Views/ClienteDetalle.xaml.cs
@@ -12,6 +12,45 @@ namespace PlacasSolares.Views
 
         }
 
+        private void OnButtonInicio(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Inicio());
+        }
+        private void OnButtonInforme(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ClienteDetalle());
+        }
+        private async void OnButtonGps(object sender, EventArgs e)
+        {
+            var location = "https://www.google.com/maps?q=37.568531, -5.428536";
+            await Launcher.OpenAsync(new Uri(location));
+        }
+        private async void OnButtonCamara(object sender, EventArgs e)
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+                if (photo != null)
+                {
+                    var stream = await photo.OpenReadAsync();
+                }
+            }
+            catch (FeatureNotSupportedException)
+            {
+                await DisplayAlert("Error", "La cámara no está soportada en este dispositivo.", "OK");
+            }
+            catch (PermissionException)
+            {
+                await DisplayAlert("Permiso denegado", "Permiso de acceso a la cámara denegado.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"Ocurrió un error: {ex.Message}", "OK");
+            }
+        }
+
+        
+
 
         private async void OnMapButtonClicked(object sender, EventArgs e)
         {
--- PlacasSolares/PlacasSolares/PlacasSolares/Views/Inicio.xaml.cs
+++ PlacasSolares/PlacasSolares/PlacasSolares/Views/Inicio.xaml.cs
@@ -13,10 +13,52 @@ public partial class Inicio : ContentPage
         
     }
 
+
+    private void OnButtonInicio(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Inicio());
+    }
+    private void OnButtonInforme(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ClienteDetalle());
+    }
+    private async void OnButtonGps(object sender, EventArgs e)
+    {
+        var location = "https://www.google.com/maps?q=37.568531, -5.428536";
+        await Launcher.OpenAsync(new Uri(location));
+    }
+    private async void OnButtonCamara(object sender, EventArgs e)
+    {
+        try
+        {
+            var photo = await MediaPicker.CapturePhotoAsync();
+            if (photo != null)
+            {
+                var stream = await photo.OpenReadAsync();
+            }
+        }
+        catch (FeatureNotSupportedException)
+        {
+            await DisplayAlert("Error", "La cámara no está soportada en este dispositivo.", "OK");
+        }
+        catch (PermissionException)
+        {
+            await DisplayAlert("Permiso denegado", "Permiso de acceso a la cámara denegado.", "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"Ocurrió un error: {ex.Message}", "OK");
+        }
+    }
+
+
     private void OnClientSelected(object sender, EventArgs e)
     {
         Navigation.PushAsync(new ClienteDetalle());
     }
+
+
+
 
 
 }
