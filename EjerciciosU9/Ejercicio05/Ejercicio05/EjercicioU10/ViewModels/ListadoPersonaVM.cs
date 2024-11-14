5aa9902e35f25261b22e6276c576a6322a655bf4
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2024-11-14 14:49:05 +0100

    me falta buscar

--- EjerciciosU9/Ejercicio05/Ejercicio05/EjercicioU10/ViewModels/ListadoPersonaVM.cs
+++ EjerciciosU9/Ejercicio05/Ejercicio05/EjercicioU10/ViewModels/ListadoPersonaVM.cs
@@ -1,4 +1,5 @@
 ﻿using BL;
+using EjercicioU10.ViewModels.Utilidades;
 using ENT;
 using System;
 using System.Collections.Generic;
@@ -11,58 +12,185 @@ using System.Threading.Tasks;
 
 namespace Ejerciciou10.ViewModels
 {
-    public class ListadoPersonaVM: INotifyPropertyChanged
+    /// <summary>
+    /// ViewModel para el listado de personas
+    /// </summary>
+    public class ListadoPersonaVM : INotifyPropertyChanged
     {
+        #region Atributos
+        // Comando para eliminar una persona
+        private DelegateCommand eliminarCommand;
+        // Comando para buscar personas
+        private DelegateCommand buscarCommand;
+        // Texto introducido en el Entry de búsqueda
+        private string textoEntry;
+        // Persona seleccionada en la lista
+        private ClsPersona personaSeleccionada;
+        // Lista observable de personas
+        private ObservableCollection<ClsPersona> listaPersonas;
+        #endregion
+
+        #region Constructor
         /// <summary>
-        /// ViewModel para el listado de personas
+        /// Constructor por defecto
         /// </summary>
-        
-            #region Atributos
-            private ClsPersona personaSeleccionada;
-            private ObservableCollection<ClsPersona> listaPersonas = new ObservableCollection<ClsPersona>(ClsListadoPersonaBl.ObtenerListadoBl());
-            #endregion
-
-            #region Constructor
-            public ListadoPersonaVM() { }
-
-            public ListadoPersonaVM(ClsPersona personaSeleccionada)
+        public ListadoPersonaVM()
+        {
+            try
             {
-                try
-                {
-                    listaPersonas = new ObservableCollection<ClsPersona>(ClsListadoPersonaBl.ObtenerListadoBl());
-                }
-                catch (Exception ex)
-                {
-                    ex.Message.ToString();
-                }
+                // Inicializa la lista de personas obtenida del negocio
+                listaPersonas = new ObservableCollection<ClsPersona>(ClsListadoPersonaBl.ObtenerListadoBl());
+                // Inicializa los comandos
+                eliminarCommand = new DelegateCommand(EliminarCommand_Executed, EliminarCommand_CanExecute);
+                buscarCommand = new DelegateCommand(BuscarCommand_Executed, BuscarCommand_CanExecute);
             }
-            #endregion
-
-            #region Propiedades
-            public ObservableCollection<ClsPersona> ListaPersonas
+            catch (Exception ex)
             {
-                get { return listaPersonas; }
-                set { listaPersonas = value; }
+                // Manejo de excepciones
+                ex.Message.ToString();
             }
-
-            public ClsPersona PersonaSeleccionada
-            {
-                get { return personaSeleccionada; }
-                set { personaSeleccionada = value; NotifyPropertyChanged("PersonaSeleccionada"); }
-            }
-            #endregion
-
-            #region Notify
-            public event PropertyChangedEventHandler? PropertyChanged;
-
-            private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
-
-            {
-
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-            }
-            #endregion
         }
-    }
 
+        /// <summary>
+        /// Constructor con parámetro de persona seleccionada
+        /// </summary>
+        /// <param name="personaSeleccionada">Persona seleccionada</param>
+        public ListadoPersonaVM(ClsPersona personaSeleccionada)
+        {
+            try
+            {
+                // Inicializa la lista de personas obtenida del negocio
+                listaPersonas = new ObservableCollection<ClsPersona>(ClsListadoPersonaBl.ObtenerListadoBl());
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+                ex.Message.ToString();
+            }
+        }
+        #endregion
+
+        #region Propiedades
+
+        /// <summary>
+        /// Comando para eliminar una persona
+        /// </summary>
+        public DelegateCommand EliminarCommand
+        {
+            get
+            {
+                return eliminarCommand;
+            }
+        }
+
+        /// <summary>
+        /// Comando para buscar personas
+        /// </summary>
+        public DelegateCommand BuscarCommand
+        {
+            get
+            {
+                return buscarCommand;
+            }
+        }
+
+        /// <summary>
+        /// Texto introducido en el Entry de búsqueda
+        /// </summary>
+        public string TextoEntry
+        {
+            get { return textoEntry; }
+            set { textoEntry = value; buscarCommand.RaiseCanExecuteChanged(); NotifyPropertyChanged("TextoEntry"); }
+        }
+
+        /// <summary>
+        /// Lista observable de personas
+        /// </summary>
+        public ObservableCollection<ClsPersona> ListaPersonas
+        {
+            get { return listaPersonas; }
+            set { listaPersonas = value; NotifyPropertyChanged("ListaPersonas"); }
+        }
+
+        /// <summary>
+        /// Persona seleccionada en la lista
+        /// </summary>
+        public ClsPersona PersonaSeleccionada
+        {
+            get { return personaSeleccionada; }
+            set { personaSeleccionada = value; eliminarCommand.RaiseCanExecuteChanged(); NotifyPropertyChanged("PersonaSeleccionada"); }
+        }
+        #endregion
+
+        #region Notify
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Notifica el cambio de una propiedad
+        /// </summary>
+        /// <param name="propertyName">Nombre de la propiedad</param>
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Métodos
+        /// <summary>
+        /// Ejecuta el comando de eliminar persona
+        /// </summary>
+        private async void EliminarCommand_Executed()
+        {
+            // Muestra una confirmación antes de eliminar
+            bool confirmacion = await ShowTask.MostrarConfirmacionAsync("¿Seguro que quieres borrar la persona?", "Confirmar borrado");
+            if (confirmacion)
+            {
+                // Elimina la persona seleccionada de la lista
+                listaPersonas.Remove(personaSeleccionada);
+            }
+        }
+
+        /// <summary>
+        /// Verifica si se puede ejecutar el comando de eliminar persona
+        /// </summary>
+        /// <returns>True si se puede eliminar, de lo contrario False</returns>
+        private bool EliminarCommand_CanExecute()
+        {
+            bool sePuedeBorrar = true;
+
+            // No se puede borrar si no hay una persona seleccionada
+            if (personaSeleccionada == null)
+            {
+                sePuedeBorrar = false;
+            }
+
+            return sePuedeBorrar;
+        }
+
+        /// <summary>
+        /// Ejecuta el comando de buscar personas
+        /// </summary>
+        private async void BuscarCommand_Executed()
+        {
+            // Implementación pendiente
+        }
+
+        /// <summary>
+        /// Verifica si se puede ejecutar el comando de buscar personas
+        /// </summary>
+        /// <returns>True si se puede buscar, de lo contrario False</returns>
+        private bool BuscarCommand_CanExecute()
+        {
+            bool sePuedeBuscar = true;
+
+            // No se puede buscar si el texto de búsqueda está vacío
+            if (string.IsNullOrEmpty(TextoEntry))
+            {
+                sePuedeBuscar = false;
+            }
+
+            return sePuedeBuscar;
+        }
+        #endregion
+    }
+}
---
+++ EjerciciosU9/Ejercicio05/Ejercicio05/EjercicioU10/ViewModels/Utilidades/ShowTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioU10.ViewModels.Utilidades
+{
+    public interface IMessageService
+    {
+        Task<bool> MostrarConfirmacion(string mensaje, string titulo);
+    }
+
+    public class ShowTask : IMessageService
+    {
+        public Task<bool> MostrarConfirmacion(string mensaje, string titulo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static async Task<bool> MostrarConfirmacionAsync(string mensaje, string titulo)
+        {
+            return await Application.Current.MainPage.DisplayAlert(titulo, mensaje, "Aceptar", "Cancelar");
+        }
+    }
+}
