17cfbbc00f4a176d69c085289f51ed7addcd776b
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-12-04 14:27:41 +0100

    hoy se gana

---
+++ ExamenBienHecho/ExamenBienHecho/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamenBienHecho
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ExamenBienHecho/ExamenBienHecho/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenBienHecho
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
