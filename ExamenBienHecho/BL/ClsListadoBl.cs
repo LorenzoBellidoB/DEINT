6c5393c226731959d656d29933f14a3726bc99e1
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-12-10 13:35:59 +0100

    daldlad

---
+++ ExamenBienHecho/BL/ClsListadoBl.cs
@@ -0,0 +1,37 @@
+﻿using DAL;
+using ENT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class ClsListadoBl
+    {
+        /// <summary>
+        /// Obtiene los candidatos para las rondas de manera aleatoria 
+        /// </summary>
+        /// <param name="rondas">Numero de rondas por partida</param>
+        /// <returns>Devuelve una lista de candidatos aleatorios</returns>
+        public static List<ClsCandidato> obtenerCandidatosAleatoriosRondasBl(int rondas)
+        {
+            List<ClsCandidato> listadoCandidatos = ClsListadoDal.obtenerCandidatos();
+
+            List<ClsCandidato> candidatosAleatorios = new List<ClsCandidato>();
+
+            candidatosAleatorios = listadoCandidatos.OrderBy(x => Guid.NewGuid()).Take(rondas).ToList();
+
+            return candidatosAleatorios;
+        }
+
+		public static List<ClsCandidato> obtenerCandidatosParaPreguntaBl(int opciones, int idCandidato)
+        {
+
+            List<ClsCandidato> listadoCandidatos = ClsListadoDal.obtenerCandidatos();
+
+
+        }
+    }
+}
---
+++ ExamenBienHecho/ENT/ClsCandidato.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT
+{
+    public class ClsCandidato
+    {
+        #region Atributos
+        private int id;
+        private string nombre;
+        #endregion
+
+        #region Propiedades
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        #endregion
+
+        #region Constructores
+        public ClsCandidato(int id, string nombre)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+        }
+        #endregion
+    }
+}
